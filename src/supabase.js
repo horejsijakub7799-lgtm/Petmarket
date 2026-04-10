fa4c72b5660a7d1723bc0b3720820862e522c280
Jakub <horejsi.jakub7799@gmail.com>
2026-04-10 12:21:05 +0200

    Supabase env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qbybizuigbweludbowuf.supabase.co'
-const supabaseKey = 'sb_publishable_iTrudcPc8hSsjHGNUcMFZQ_zuFHGZen'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://qbybizuigbweludbowuf.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_iTrudcPc8hSsjHGNUcMFZQ_zuFHGZen'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
