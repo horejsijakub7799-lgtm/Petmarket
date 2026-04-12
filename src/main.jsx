6a16ed4ef08f087ac3209ca1850c5e0170044014
Jakub <horejsi.jakub7799@gmail.com>
2026-04-12 15:21:51 +0200

    add vencitel route

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx'
 import ProfilePage from './ProfilePage.jsx'
 import VetRegister from './VetRegister.jsx'
 import HotelRegister from './HotelRegister.jsx'
+import VencitelRegister from './VencitelRegister.jsx'
 import PartneriPage from './PartneriPage.jsx'
 import { AuthProvider } from './useAuth.jsx'
 
@@ -18,6 +19,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/profil" element={<ProfilePage />} />
           <Route path="/veterinar/registrace" element={<VetRegister />} />
           <Route path="/hotel/registrace" element={<HotelRegister />} />
+          <Route path="/vencitel/registrace" element={<VencitelRegister />} />
           <Route path="/partneri" element={<PartneriPage />} />
         </Routes>
       </BrowserRouter>
