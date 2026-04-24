c00af7bf733a04cd91f1c66fc5cf3c46ca0c3ab5
Jakub <horejsi.jakub7799@gmail.com>
2026-04-24 12:43:08 +0200

    Add vycvik routes to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,14 @@ import ProfilePage from './ProfilePage.jsx'
 import VetRegister from './VetRegister.jsx'
 import HotelRegister from './HotelRegister.jsx'
 import VencitelRegister from './VencitelRegister.jsx'
+import VycvikRegister from './VycvikRegister.jsx'
 import ProdejceRegister from './ProdejceRegister.jsx'
 import PartneriPage from './PartneriPage.jsx'
 import AdminPage from './AdminPage.jsx'
 import VeterinariPage from './VeterinariPage.jsx'
 import HotelyPage from './HotelyPage.jsx'
 import VenceniPage from './VenceniPage.jsx'
+import VycvikPage from './VycvikPage.jsx'
 import SellerDashboard from './SellerDashboard.jsx'
 import ShopPage from './ShopPage.jsx'
 import PojisteniPage from './PojisteniPage.jsx'
@@ -21,6 +23,7 @@ import PartnerDashboard from './PartnerDashboard.jsx'
 import RadyPage from './RadyPage.jsx'
 import RadyDetailPage from './RadyDetailPage.jsx'
 import { AuthProvider } from './useAuth.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
@@ -31,12 +34,14 @@ createRoot(document.getElementById('root')).render(
           <Route path="/veterinar/registrace" element={<VetRegister />} />
           <Route path="/hotel/registrace" element={<HotelRegister />} />
           <Route path="/vencitel/registrace" element={<VencitelRegister />} />
+          <Route path="/vycvik/registrace" element={<VycvikRegister />} />
           <Route path="/prodejce/registrace" element={<ProdejceRegister />} />
           <Route path="/partneri" element={<PartneriPage />} />
           <Route path="/admin-x7k9p2" element={<AdminPage />} />
           <Route path="/veterinari" element={<VeterinariPage />} />
           <Route path="/hotely" element={<HotelyPage />} />
           <Route path="/venceni" element={<VenceniPage />} />
+          <Route path="/vycvik" element={<VycvikPage />} />
           <Route path="/pojisteni" element={<PojisteniPage />} />
           <Route path="/seller/dashboard" element={<SellerDashboard />} />
           <Route path="/shop" element={<ShopPage />} />
